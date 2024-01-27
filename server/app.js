d237a9b7d8090ad5c628cd2b039536ca852380b8
lvnvn177 <vheh502@gmail.com>
2024-01-27 17:10:58 +0900

    20240127_1

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const router = require('./routes');
 const { upload, uploadFileToS3 } = require('./aws-s3_FileUpload');
+const { spawn } = require ('child_process');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
@@ -30,7 +31,24 @@ app.post('/upload', upload.single('file'), (req, res) => {
     res.json({ message: 'File uploaded successfully!' });
 });
 
+const startFlaskServer = () => {
+    const flaskProcess = spawn('python', ['./learn_server.py']);
+
+    flaskProcess.stdout.on('data', (data) => {
+        console.log('Flask server print: ${data');
+    });
+
+    flaskProcess.stderr.on('data', (data) => {
+        console.error('Flask 서버 오류: ${data}');
+    });
+
+    flaskProcess.on('close', (code) => {
+        console.log('Flask 서버가 코드 ${code}로 종료되었습니다.');
+    });
+};
+
 // Startup
 app.listen(PORT, () => {
+    startFlaskServer();
     console.log(`The API Server is listening on port: ${PORT}`);
 });
