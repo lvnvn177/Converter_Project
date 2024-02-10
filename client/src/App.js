2ad6197316413e652a1fd5fdf048200d8bc85042
lvnvn177 <vheh502@gmail.com>
2024-02-11 00:18:42 +0900

    frontend_module_add_0210

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,51 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/nav.js';
-import Dropbox from './components/box.js';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <>   
-    <div className="App">
-      <Navigation />
-      </div>
 
-      <div className="vertical-space"></div>
-      <h3 className="center-text">Extract tables from PDF/Images</h3>
-      <div className="vertical-space"></div>
-      <Dropbox />
-      
-      </>   
-   
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  const handleFileUpload = async () => {
+    try{
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      await axios.post('/upload', formData);
+      console.log('File uploaded successfully!');
+    }catch(error){
+      console.error('File uploaded failed:', error);
+    }
+  };
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* 파일 선택 input */}
+        <input type="file" onChange={handleFileChange}/>
+
+        {/* 파일 업로드 버튼 */}
+        <button onClick={handleFileUpload}>Upload File</button>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
