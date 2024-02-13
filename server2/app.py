115bc943bc9cb7838089f77bfa47141e8184325d
lvnvn177 <vheh502@gmail.com>
2024-02-13 09:44:42 +0900

    sharp_20240213

--- server2/app.py
+++ server2/app.py
@@ -1,4 +1,29 @@
-"""""
+from fastapi import FastAPI, APIRouter, File, UploadFile
+import uvicorn
+from detector import Detector
+from model import Model
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+router = APIRouter()
+detector = Detector()
+
+@router.get("/")
+async def home():
+    return {"message": "Model service"}
+
+@router.post("/detection")
+async def predict(file: UploadFile = File(...)):
+    return detector.get_result(image_file=file)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", reload=True, port=6000, host="127.0.0.1")
+
+""""
 from fastapi import FastAPI, File, UploadFile
 from detector import Detector
 import uvicorn
@@ -17,12 +42,13 @@ async def home():
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     # 여기서 파일을 처리하고 응답을 반환합니다.
+    ##return {"message": "File received"}
     return detector.get_result(image_file=file)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True, port=6000, host="127.0.0.1")
-"""""
-
+"""
+"""
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 import logging
@@ -39,3 +65,4 @@ async def predict(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True, port=6000, host="127.0.0.1")
+    """
